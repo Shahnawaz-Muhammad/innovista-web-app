715c102acb9e91558f818347442738b45ccdf1dd
FahadKhan111 <ak0902586@gmail.com>
2023-11-16 17:53:18 +0500

    tab details

--- src/pages/user/user-tabs/Education.jsx
+++ src/pages/user/user-tabs/Education.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 const Education = () => {
   return (
-    <div>Education</div>
-  )
-}
+    <div className=" p-3 lg:p-3  ">
+      <div className="pt-2 lg:grid grid-cols-5 gap-4">
+        {/* First Row */}
+        <div className='col-span-1  py-6'>
+          
+          <p className='text-[#adadad] text-sm'>Education</p>
+        </div>
+        <div className=' pt-6 lg:col-span-2'>
+          <h1>Ms Software Engineering.</h1>
+          <p className='text-[#adadad] text-sm '>Completed 2009</p>
+        </div>
+        <div className='pt-6 lg:col-span-2'>
+          <h1>Fsc Pre Engineering</h1>
+          <p className='text-[#adadad] text-sm'>Completed 2005</p>
+        </div>
 
-export default Education
+        {/* Second Row */}
+        <div className=''>
+          <h1></h1>
+          <p></p>
+        </div>
+        <div className='pt-2 lg:col-span-2'>
+          <h1>Bs Software Engineering</h1>
+          <p className='text-[#adadad] text-sm'>Completed 2007</p>
+        </div>
+        <div className='pt-2 lg:col-span-2'>
+          <h1>Matric in Computer Science</h1>
+          <p className='text-[#adadad] text-sm'>Completed 2003</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
