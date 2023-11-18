2d27b9b3c3a00f65942be8f0d1cf76b4941e33d6
Shahnawaz-Muhammad <shahnawazbaltee55@gmail.com>
2023-11-18 15:29:33 +0500

    login - registration submit type changes

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -35,6 +35,7 @@ const Login = () => {
                 name="email"
                 id="email"
                 placeholder="name@company.com"
+                // {...register("email", { required: true })}
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required=""
               />
@@ -51,10 +52,14 @@ const Login = () => {
                 name="password"
                 id="password"
                 placeholder="••••••••"
+                // {...register("password", { required: true })}
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required=""
               />
             </div>
+            {/* <div>
+              <p>{error}</p>
+            </div> */}
             <div className="flex items-center justify-between">
               <div className="flex items-start">
                 <div className="flex items-center h-5">
@@ -83,13 +88,13 @@ const Login = () => {
               </Link>
             </div>
             <button
-              type="button"
+              type="submit"
               className="w-full text-white bg-orange hover:bg-orangeDark focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
               Sign in
             </button>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              Don`t have an account yet?{" "}
+              Don’t have an account yet?{" "}
               <Link
                 to="/register"
                 className="font-medium text-primary-600 hover:underline dark:text-primary-500"
