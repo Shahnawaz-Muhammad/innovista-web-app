3892a61a3fe169a6b373108ddc5df5278b1dace4
Wasim21a <wasimmushtaq136@gmail.com>
2023-11-18 14:17:46 +0500

    Updated Bio

--- src/pages/user/user-tabs/Bio.jsx
+++ src/pages/user/user-tabs/Bio.jsx
@@ -10,44 +10,44 @@ const Bio = () => {
             "url('https://images.unsplash.com/photo-1621243804936-775306a8f2e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80')",
         }}
       >
-<button className="p-3 rounded-lg text-center text-4xl font-extrabold text-white bg-orange">Advance Booking</button>
+        <button className="p-3 rounded-lg text-center text-4xl font-extrabold text-white bg-orange">
+          Advance Booking
+        </button>
       </div>
 
       <div className="w-full md:w-2/3 border-2 border-orange  ">
         <div className="p-3 grid grid-cols-1 lg:grid-cols-2 gap-4">
-          
-            <div className="flex flex-row ">
-              <h1 className="font-bold w-1/3">First Name</h1>
-              <h1 className="font-medium w-2/3">Charles</h1>
-            </div>
-            <div className="flex flex-row ">
-              <h1 className="font-bold w-1/3">Last Name</h1>
-              <h1 className="font-medium w-2/3">Cater</h1>
-            </div>
-            <div className="flex flex-row ">
-              <h1 className="font-bold w-1/3">Address</h1>
-              <h1 className="font-medium w-2/3">7188 E. Walnutwood St. Brooklyn, NY 11238</h1>
-            </div>
-            <div className="flex flex-row ">
-              <h1 className="font-bold w-1/3">Date of Birth</h1>
-              <h1 className="font-medium w-2/3">11/05/1997</h1>
-            </div>
-            <div className="flex flex-row ">
-              <h1 className="font-bold w-1/3">Email</h1>
-              <h1 className="font-medium w-2/3">charlescater01@gmail.com</h1>
-            </div>
-            <div className="flex flex-row ">
-              <h1 className="font-bold w-1/3">Phone No</h1>
-              <h1 className="font-medium w-2/3">091 25 2554 255</h1>
-            </div>
-            <div className="flex flex-row ">
-              <h1 className="font-bold w-1/3">CNIC</h1>
-              <h1 className="font-medium w-2/3">611101-542544-5</h1>
-            </div>
-            
-            
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">First Name</h1>
+            <h1 className="font-medium w-2/3">Charles</h1>
+          </div>
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">Last Name</h1>
+            <h1 className="font-medium w-2/3">Cater</h1>
+          </div>
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">Address</h1>
+            <h1 className="font-medium w-2/3">
+              7188 E. Walnutwood St. Brooklyn, NY 11238
+            </h1>
+          </div>
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">Date of Birth</h1>
+            <h1 className="font-medium w-2/3">11/05/1997</h1>
+          </div>
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">Email</h1>
+            <h1 className="font-medium w-2/3">charlescater01@gmail.com</h1>
+          </div>
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">Phone No</h1>
+            <h1 className="font-medium w-2/3">091 25 2554 255</h1>
+          </div>
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">CNIC</h1>
+            <h1 className="font-medium w-2/3">611101-542544-5</h1>
+          </div>
         </div>
-        
       </div>
     </div>
   );
