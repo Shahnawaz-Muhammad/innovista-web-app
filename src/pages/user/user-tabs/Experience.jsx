1489b5ec7a48c57ddd3ac5ede1b3205cb6403db4
Wasim21a <wasimmushtaq136@gmail.com>
2023-11-20 13:18:35 +0500

    Updated Experience

--- src/pages/user/user-tabs/Experience.jsx
+++ src/pages/user/user-tabs/Experience.jsx
@@ -3,58 +3,51 @@ import React from "react";
 const Experience = () => {
   return (
     <div>
-      <div className="flex lg:w-[30%] justify-between px-2">
-        <h1 className="mt-3 font-bold">Total Experience Of Field</h1>
-        <h1 className="text-4xl text-[#f99904]">
-          12<span className="text-sm">years</span>
-        </h1>
-      </div>
-      <div className="lg:w-[30%] h-0.5 mt-2 bg-[#e2e2e2]"></div>
+      <div className="bg-[#fffbf5] rounded-3xl flex flex-col md:flex-row justify-center">
+      <div
+        className="w-full  md:w-1/3 flex justify-center items-center py-5"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1621243804936-775306a8f2e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80')",
+        }}
+      >
+        <div className="flex flex-col gap-4">
 
-      <h1 className="py-6 px-2 text-[#f99904] font-semibold">Mistry IT Company</h1>
-
-    
-      <div className="lg:grid grid-cols-3 px-2">
-      
-        <div className="flex col-span-1">
-          <div className=" ">
-            <p className="text-[#adadad] text-sm">Total Experience</p>
-          </div>
-          <div className=" px-10 lg:px-8 ">
-            <h1 className="font-semibold">14 Month</h1>
-            <p className="text-[#adadad] text-sm ">At Mistry IT</p>
-          </div>
-        </div>
-
-        <div className="flex col-span-2">
-          <div className=" ">
-            <p className="text-[#adadad] text-sm">Start Date</p>
-          </div>
-          <div className=" px-20 lg:px-8  ">
-            <h1 className="font-semibold">Start October 2022</h1>
-            <p className="text-[#adadad] text-sm ">At Mistry IT</p>
-          </div>
-        </div>
-
-        <div className="flex col-span-1 pt-8">
-          <div className=" ">
-            <p className="text-[#adadad] text-sm">Positions</p>
-          </div>
-          <div className="  px-20 lg:px-20 ">
-            <h1 className="font-semibold">Senior Software Developer</h1>
-            <p className="text-[#adadad] text-sm ">At Mistry IT</p>
-          </div>
-        </div>
-        <div className="flex col-span-2 pt-8">
-          <div className=" ">
-            <p className="text-[#adadad] text-sm">End Date</p>
-          </div>
-          <div className="  px-20 lg:px-8  ">
-            <h1 className="font-semibold">Continue</h1>
-            <p className="text-[#adadad] text-sm ">At Mistry IT</p>
-          </div>
+        <button className="p-3 rounded-lg text-center text-4xl font-extrabold text-white bg-orange">
+        Work Experience
+        </button>
+        <button className="p-3 rounded-lg text-center text-4xl font-extrabold text-white bg-orange">
+        12 Years
+        </button>
         </div>
       </div>
+
+      <div className="w-full md:w-2/3 border-2 border-orange py-5 ">
+      <h1 className="text-3xl text-center text-orange font-bold underline decoration-slate-900">Mr. IT Company</h1>
+        <div className="p-3 grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">Total Experience</h1>
+            <h1 className="font-medium w-2/3">14 Months</h1>
+          </div>
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">Designation</h1>
+            <h1 className="font-medium w-2/3">Senior Software Developer</h1>
+          </div>
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">Start Date</h1>
+            <h1 className="font-medium w-2/3">
+            Aug 2022
+            </h1>
+          </div>
+          <div className="flex flex-row ">
+            <h1 className="font-bold w-1/3">End Date</h1>
+            <h1 className="font-medium w-2/3">Oct 2023</h1>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
     </div>
   );
 };
